ff45cfb119f97f5066a7fb53f3707513dca97a7a
Yolande <yolande.hattingh@advance.io>
2022-05-27 09:39:45 +0200

    LoginPracticeTests:  Move and update method to read from Excel

--- LogBox20220513/src/test/java/pageObjectsLogBox/PageObjectsBrochurePage.java
+++ LogBox20220513/src/test/java/pageObjectsLogBox/PageObjectsBrochurePage.java
@@ -1,31 +1,57 @@
 package pageObjectsLogBox;
+
 import java.io.IOException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import frameWork.BasePageFrameWork;
+import frameWork.FileUtilities;
+
 public class PageObjectsBrochurePage extends BasePageFrameWork {
+
+	String inputDirectory = getDataConfigProperties("inputDir");
+	String inputFile = inputDirectory + "logBoxUserAccounts.xlsx";
+
 	// Method:Select Practice on Brochure Page
 	public void selectPracticeAndClickLoginButton() throws InterruptedException {
-		driver.wait(200);
-		driver.findElement(By.cssSelector(".navbar-nav [href='\\#\\/practice']")).click();
-		driver.findElement(By.linkText("LOGIN")).click();
+		driver.findElement(By.xpath("//div[@id='container']/nav//a[@href='#/login']")).click();
+		waitForElement(100, By.cssSelector("[src='\\/img\\/Practice_Text\\.b772cae\\.png']"));
+		driver.findElement(By.cssSelector("[src='\\/img\\/Practice_Text\\.b772cae\\.png']")).click();
 	}
-	
-	//Method:  Click Login Button
+
+	// Method: Click Login Button
 	public void clickLoginButtonToSubmitUsernameAndPassword() {
 		driver.findElement(By.cssSelector("#login-submit")).click();
-		
+
 	}
+
 	// Method: To Test if Values are Read from Excel File with Username and Password
-	//@Test(dataProvider = "logBoxUserAccount", dataProviderClass = ReadDataFromExcel.class)
+	// @Test(dataProvider = "logBoxUserAccount", dataProviderClass =
+	// ReadDataFromExcel.class)
 	public void testExcelReader(String username, String password) {
 		System.out.println(username + " " + password);
 	}
+
 	// Method: To Login with Username and Password parameters
 	public void loginUsernamePassword(String username, String password) throws IOException {
 		driver.findElement(By.id("username")).sendKeys(username);
 		driver.findElement(By.id("password")).sendKeys(password);
 	}
-	
-	
+
+	// Method: To input Username and Password from Excel
+	public void insertUsernameAndPasswordFromExcel(int rowNumber, int cellNumber1, int cellNumber2) throws IOException {
+
+		FileUtilities fileUtilities = new FileUtilities();
+
+		WebElement username = driver.findElement(By.id("username"));
+		WebElement password = driver.findElement(By.id("password"));
+
+		// Method: To call the FileUtilities method to read from Excel
+		fileUtilities.setExcelFile(inputFile, "sheet1");
+		for (int i = rowNumber; i <= fileUtilities.getRowCountInSheet(); i++) {
+			username.sendKeys(fileUtilities.getCellData(i, cellNumber1));
+			password.sendKeys(fileUtilities.getCellData(i, cellNumber2));
+
+		}
+	}
 }
