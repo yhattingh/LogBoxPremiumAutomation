265c667dc9ff1d947caf21bbded4edb5ec36597d
Yolande <yolande.hattingh@advance.io>
2022-05-28 13:27:38 +0200

    PageObjectsHomePage:  Add searchPracticePations methods ; HomePageTests:  Add shouldSearchForPatient test

---
+++ LogBox20220513/src/test/java/testsLogBox/ActivityTests.java
@@ -0,0 +1,32 @@
+package testsLogBox;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.hamcrest.core.StringContains;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import frameWork.BasePageFrameWork;
+import pageObjectsLogBox.PageObjectsBrochurePage;
+import pageObjectsLogBox.PageObjectsHomePage;
+
+public class ActivityTests extends BasePageFrameWork{
+	
+	PageObjectsHomePage pageObjectsHomePage = new PageObjectsHomePage();
+	PageObjectsBrochurePage pageObjectsBrochurePage = new PageObjectsBrochurePage();
+	
+	public void shouldSearchForPatient() throws InterruptedException, IOException {
+		pageObjectsBrochurePage.selectPracticeAndClickLoginButton();
+		pageObjectsBrochurePage.insertUsernameAndPasswordFromExcel(1,1,2);
+    	pageObjectsBrochurePage.clickLoginButtonToSubmitUsernameAndPassword();
+		pageObjectsHomePage.searchPracticePatientsOnHomePage("John");
+		pageObjectsHomePage.clickOnSearchedPatient();
+		String feedURL = driver.getCurrentUrl();
+		Assert.assertTrue(feedURL.contains("feed"));
+		Reporter.log("User ");
+	}
+
+}
---
+++ LogBox20220513/src/test/java/testsLogBox/HomePageTests.java
@@ -0,0 +1,35 @@
+package testsLogBox;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.hamcrest.core.StringContains;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import frameWork.BasePageFrameWork;
+import pageObjectsLogBox.PageObjectsBrochurePage;
+import pageObjectsLogBox.PageObjectsHomePage;
+
+public class HomePageTests extends BasePageFrameWork{
+	
+	PageObjectsHomePage pageObjectsHomePage = new PageObjectsHomePage();
+	PageObjectsBrochurePage pageObjectsBrochurePage = new PageObjectsBrochurePage();
+	
+	@Test
+	public void shouldSearchForPatient() throws InterruptedException, IOException {
+		pageObjectsBrochurePage.selectPracticeAndClickLoginButton();
+		pageObjectsBrochurePage.insertUsernameAndPasswordFromExcel(1,1,2);
+    	pageObjectsBrochurePage.clickLoginButtonToSubmitUsernameAndPassword();
+		pageObjectsHomePage.searchPracticePatientsOnHomePage("John");
+		WebElement searchedPatient = driver.findElement(By.cssSelector("[data-cy] tr td:nth-of-type(2)"));
+		//searchedPatient.toString();
+		//Assert.assertTrue(searchedPatient.contains("John"));
+		
+	}
+	
+}
