c504bb6fa7fa987b5937a86014b3347659fcc1cb
Yolande <yolande.hattingh@advance.io>
2022-06-12 18:31:55 +0200

    ActivityTests:  completed user story - shouldCreateActivityForSelectedPatient

--- LogBox20220513/src/test/java/pageObjectsLogBox/PageObjectsActivityPage.java
+++ LogBox20220513/src/test/java/pageObjectsLogBox/PageObjectsActivityPage.java
@@ -42,73 +42,58 @@ public class PageObjectsActivityPage extends BasePageFrameWork {
 		return textFromCaseFilesList;
 	}
 
-//	WebElement select = driver.findElement(By.id("gender"));
-//	List<WebElement> options = select.findElements(By.tagName("Male")); 
-//	for (WebElement option : options) ...
-
-	public static void anotherList(String[] args) {
-		// List<WebElement> l = driver.findElements(By.className("v-select-list v-card
-		// theme--light"));
-		// List<WebElement> l = driver.findElements(By.className("v-menu__content
-		// theme--light menuable__content__active"));
-		List<WebElement> l = driver.findElements(By.className("selectCaseFileListItem"));
-		System.out.println("Elements are: ");
-		System.out.println("size = :" + l.size());
-		System.out.println("listIterator === " + l);
-		for (int j = 0; j < l.size(); j++) {
-			// identify separate div
-			String v = l.get(j).getText();
-			System.out.println("Elements are: ");
-			System.out.println(v);
-		}
-	}
-
-	
-		public void selectItemFromDropdownList(String caseFileName) {
+	public void selectAnItemInDropdownList(String caseFileName) {
 
 		// Click on Dropdown
 		driver.findElement(By.className("v-select__selections"));
-		
+
 		List<WebElement> allOptions = driver.findElements(By.className("selectCaseFileListItem"));
 		System.out.println("number of returned rows: " + " " + allOptions.size());
-		
+
 		for (int i = 0; i <= allOptions.size() - 1; i++) {
 			System.out.println(allOptions.get(i).getText());
-			
+
 			if (allOptions.get(i).getText().contains(caseFileName)) {
 				allOptions.get(i).click();
 				break;
 			}
 		}
 	}
-		
-		public void selectItemFromDropdownList1(String caseFileName) {
-			
-		JavascriptExecutor je = (JavascriptExecutor) driver;
 
-		WebElement element = driver.findElement(By.className("v-select__selections"));
-		System.out.println("number of returned rows: " + " " + element.getSize());
-		
-		je.executeScript("javascript:window.scrollBy(0,250)");
-//		je.executeScript("arguments[0].scrollIntoViewIfNeeded(true);",element);
-//		for (int i = 0; i <= element.size() - 1; i++) {
-			System.out.println("show element text" + " " + element.getText());
-		if (element.getText().contains(caseFileName)) {
-			element.click();
-		}
-		
-		System.out.println("What is the element?" + " " + element.getText());
-		
-		//https://stackoverflow.com/questions/3401343/scroll-element-into-view-with-selenium
-		//https://www.tutorialspoint.com/scroll-element-into-view-with-selenium
-			
-			}
+
+	public void selectItemFromDropdownList(String caseFileName) {
+
+		WebElement element = driver.findElement(By.xpath("//*[contains(text(),'" + caseFileName + "')]"));
+
+		clickElement(By.xpath("//*[contains(text(),'" + caseFileName + "')]"));
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+		System.out.println("Which case file was selected" + " " + getTextFromFirstItemOnCaseFilesList());
+
 	}
+	
+		//https://www.tutorialspoint.com/scroll-element-into-view-with-selenium
 
 
-////Create instance of Javascript executor				
-//JavascriptExecutor je = (JavascriptExecutor) driver;				
-////Identify the WebElement which will appear after scrolling down				
-//WebElement element = driver.findElement(By.tagName("...."));				
-////now execute query which actually will scroll until that element is not appeared on page.				
-//je.executeScript("arguments[0].scrollIntoView(true);",element);
+	
+	public void clickAddActivityButton() {
+		clickElement(By.cssSelector(".mx-0.primary--text.theme--light.v-btn.v-btn--outlined.v-size--small > .v-btn__content"));
+	}
+	
+	public void addTextNote(String enterTextNote) throws InterruptedException {
+		enterText(By.cssSelector("[rows]"), enterTextNote);
+	}
+	
+	public void clickPostButton() {
+		clickElement(By.cssSelector("button#add-activity-submit-btn-id > .v-btn__content"));
+	}
+	
+	public String getTextFromActivityNote() throws InterruptedException {
+		waitForElement(1500, By.cssSelector("div:nth-of-type(1) > .mb-3.pb-2.theme--light.v-card.v-sheet > .no-gutters.row > .v-card__text  .col.pr-3"));
+		String textFromActivityNote = getElementText(By.cssSelector("div:nth-of-type(1) > .mb-3.pb-2.theme--light.v-card.v-sheet > .no-gutters.row > .v-card__text  .col.pr-3"));
+		return textFromActivityNote;
+	}
+}
+
+
